9c9917224d7b71d988626ce12cd86a1ff93ba60b
Shaurya Nanda <122524442+shauryananda@users.noreply.github.com>
2024-04-23 18:23:02 +0530

    Add files via upload

---
+++ SQL/Operation_Analytics_Database.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE sql_trainity_operation_analytics_and_investigating_metric_spike;
+SHOW DATABASES;
+
+USE sql_trainity_operation_analytics_and_investigating_metric_spike;
+
+/* Case Study 1: Job Data Analysis */
+
+CREATE TABLE job_data 
+( ds DATE,
+  job_id INT NOT NULL,
+  actor_id INT NOT NULL,
+  event VARCHAR(15) NOT NULL,
+  language VARCHAR(15) NOT NULL,
+  time_spent INT NOT NULL,
+  org CHAR(2)
+);
+
+INSERT INTO job_data (ds, job_id, actor_id, event, language, time_spent, org)
+VALUES('2020-11-30', 21, 1001, 'skip', 'English', 15, 'A'),
+      ('2020-11-30', 22, 1006, 'transfer', 'Arabic', 25, 'B'),
+      ('2020-11-29', 23, 1003, 'decision', 'Persian', 20, 'C'),
+      ('2020-11-28', 23, 1005, 'transfer', 'Persian', 22, 'D'),
+      ('2020-11-28', 25, 1002, 'decision', 'Hindi', 11, 'B'),
+      ('2020-11-27', 11, 1007, 'decision', 'French', 104, 'D'),
+      ('2020-11-26', 23, 1004, 'skip', 'Persian', 56, 'A'),
+      ('2020-11-25', 20, 1003, 'transfer', 'Italian', 45, 'C');
+      
+SELECT * FROM job_data;
+      
+
+
+
